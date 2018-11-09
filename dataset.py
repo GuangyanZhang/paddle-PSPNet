d2b7cf52e5875d3aa312808cee80afb9b732c668
mwsht <ht@mws.site>
2018-11-09 23:38:49 +0800

    dataset

---
+++ dataset.py
@@ -0,0 +1,50 @@
+import numpy as np
+import os
+
+from PIL import Image
+
+
+EXTENSIONS = ['.jpg', '.png']
+
+def load_image(file):
+    return Image.open(file)
+
+def is_image(filename):
+    return any(filename.endswith(ext) for ext in EXTENSIONS)
+
+def image_path(root, basename, extension):
+    return os.path.join(root, basename + extension)
+
+def image_basename(filename):
+    return os.path.basename(os.path.splitext(filename)[0])
+
+class VOC12:
+
+    def __init__(self, root, input_transform=None, target_transform=None):
+        self.images_root = os.path.join(root, 'images')
+        self.labels_root = os.path.join(root, 'classes')
+
+        self.filenames = [image_basename(f)
+            for f in os.listdir(self.labels_root) if is_image(f)]
+        self.filenames.sort()
+
+        self.input_transform = input_transform
+        self.target_transform = target_transform
+
+    def getitem(self, index):
+        filename = self.filenames[index]
+
+        with open(image_path(self.images_root, filename, '.jpg'), 'rb') as f:
+            image = load_image(f).convert('RGB')
+        with open(image_path(self.labels_root, filename, '.png'), 'rb') as f:
+            label = load_image(f).convert('P')
+
+        if self.input_transform is not None:
+            image = self.input_transform(image)
+        if self.target_transform is not None:
+            label = self.target_transform(label)
+
+        return image, label
+
+    def len(self):
+        return len(self.filenames)
