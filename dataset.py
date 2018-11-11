48e31bce76805e71c397b8a3a64243b7b1b45e82
mwsht <ht@mws.site>
2018-11-11 20:38:48 +0800

    saving and readinhg dataset of pkl file

--- dataset.py
+++ dataset.py
@@ -1,8 +1,8 @@
-import numpy as np
 import os
+import pickle
 
+import numpy as np
 from PIL import Image
-
 
 EXTENSIONS = ['.jpg', '.png']
 
@@ -15,12 +15,12 @@ def is_image(filename):
 def image_path(root, basename, extension):
     return os.path.join(root, basename + extension)
 
-def image_basename(filename):
+def image_basename(filename,):
     return os.path.basename(os.path.splitext(filename)[0])
 
-class VOC12:
+class VOC:
 
-    def __init__(self, root, input_transform=None, target_transform=None):
+    def __init__(self, root, img_pkl_path = 'img.pkl', lab_pkl_path = 'lab.pkl', input_transform=None, target_transform=None):
         self.images_root = os.path.join(root, 'images')
         self.labels_root = os.path.join(root, 'classes')
 
@@ -30,6 +30,24 @@ class VOC12:
 
         self.input_transform = input_transform
         self.target_transform = target_transform
+        self.img = []
+        self.lab = []
+        if os.path.isfile(img_pkl_path):
+            print '[info] loading datasets with saved pkl file.'
+            load_img = open(img_pkl_path, 'rb')
+            load_lab = open(lab_pkl_path, 'rb')
+            for _ in range(self.len()):
+                self.img.append(pickle.load(load_img))
+                self.lab.append(pickle.load(load_lab))
+        else:
+            dump_img = open(img_pkl_path, 'wb')
+            dump_lab = open(lab_pkl_path, 'wb')
+            for i in range(self.len()):
+                img, lab = self.getitem(i)
+                pickle.dump(img, dump_img, -1)
+                pickle.dump(lab, dump_lab, -1)
+                if (i%10 == 0):
+                    print(i/10)
 
     def getitem(self, index):
         filename = self.filenames[index]
